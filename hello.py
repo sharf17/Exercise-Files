5a8ac57a3cfd866bbde70cde0f187f27b8bca7e6
sharf <mohammedsharfuddin390@gmail.com>
2021-07-16 19:49:00 +0530

    updated

--- hello.py
+++ hello.py
@@ -2,7 +2,5 @@ name = input("What should you be called as?")
 print('Hello ', name)
 for i in name:
     print(i)
-desc = """ssdisihsdsi,
-sdvjsdovjsdoj,
-smcksdjoicj"""
-print(desc)
+desc = "Hello Miss Sujata!"
+print(desc[3])
