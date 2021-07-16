104fb70635fa72aa49227417f4416d302f05f69b
sharf <mohammedsharfuddin390@gmail.com>
2021-07-16 20:19:15 +0530

    updated

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
-name = input("What should you be called as?")
-print('Hello ', name)
-for i in name:
-    print(i)
-# desc = "Hello Miss Sujata!"
-# print(desc[3])
-txt = "Hello sujata ji, hope you are doing well!"
-print('sujata' in txt)
-if 'seed' in txt:
-    print('sujata')
-else:
-    print('correct your element')
-print('a')
+# name = input("What should you be called as?")
+# print('Hello ', name)
+# for i in name:
+#     print(i)
+# # desc = "Hello Miss Sujata!"
+# # print(desc[3])
+# txt = "Hello sujata ji, hope you are doing well!"
+# print('sujata' in txt)
+# if 'seed' in txt:
+#     print('sujata')
+# else:
+#     print('correct your element')
+# print('a')
+a = "sujata is a dreamer!"
+print(a[2:9:2])
