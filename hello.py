bdb73b66656afe3043dc9088b3c81583fcf9dc4c
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-17 15:32:23 +0530

    Updated

--- hello.py
+++ hello.py
@@ -23,5 +23,5 @@
 # place = 'ajanale'
 # detail = 'Hey I am {2}, I am {1} years old and I live at {0}'
 # print(detail.format(place,age,name))
-name = "Hey we are \"Indian\""
-print(name)
+name = "hey we are \"Indian\""
+print(name.upper())
