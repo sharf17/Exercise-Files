9ddc2055bbb55d5ce3ff37af673ec2bf477d8bc8
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-17 15:14:20 +0530

    updates

--- hello.py
+++ hello.py
@@ -13,6 +13,13 @@
 # print('a')
 # a = "    Sujata Rajaram Pujari!"
 # print(a.strip())
-a = 10
-b = 'sujju {}'
-print (b.format(a))
+# a = 10
+# b = 'sujju {}'
+# print (b.format(a))
+# a = 'heelo world'
+# print(a.split())
+name = 'sujata' #0
+age = 23 #1
+place = 'ajanale' #2
+detail = 'Hey I am {0}, I am {1} years old and I live at {2}'
+print(detail.format(name,age,place))
