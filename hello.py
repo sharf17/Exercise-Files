275f152cce649b77f38e24350b87fdfc5ca332a8
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-21 03:10:27 +0530

    Updates

--- hello.py
+++ hello.py
@@ -4,8 +4,8 @@ for i in name:
     print(i)
 desc = "Hello Miss Sujata!"
 print(desc[3])
-txt = "Hello sujata ji, hope you are doing well!"
-print('sujata' in txt)
+# txt = "Hello sujata ji, hope you are doing well!"
+# print('sujata' in txt)
 # if 'seed' in txt:
 #     print('sujata')
 # else:
