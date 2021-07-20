ef4fe6ec651a394c85f48964069c36fb9dc85a0e
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-21 03:09:20 +0530

    Updates

--- hello.py
+++ hello.py
@@ -22,7 +22,7 @@ name = 'sujata'
 age = 23 
 place = 'ajanale'
 detail = 'Hey I am {2}, I am {1} years old and I live at {0}'
-# print(detail.format(place,age,name))
+print(detail.format(place,age,name))
 # name = "Hey we are Indian's"
 # a = name.upper()
 # print(name.count('a'))
