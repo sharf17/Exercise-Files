8e1d92e781c7e6ecace1fa0a519e2bd94de8b9d8
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-21 03:09:11 +0530

    Updates

--- hello.py
+++ hello.py
@@ -21,7 +21,7 @@ print(a.split())
 name = 'sujata' 
 age = 23 
 place = 'ajanale'
-# detail = 'Hey I am {2}, I am {1} years old and I live at {0}'
+detail = 'Hey I am {2}, I am {1} years old and I live at {0}'
 # print(detail.format(place,age,name))
 # name = "Hey we are Indian's"
 # a = name.upper()
