dc63f47b0bd905e6b80fa5892a173ad457a1e991
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-22 17:10:05 +0530

    Updated

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@ name = input("What should you be called as?")
 print('Hello ', name)
 for i in name:
     print(i)
-desc = "Hello Miss Sujata!"
-print(desc[3])
+# desc = "Hello Miss Sujata!"
+# print(desc[3])
 # txt = "Hello sujata ji, hope you are doing well!"
 # print('sujata' in txt)
 # if 'seed' in txt:
