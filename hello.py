260801f992278cb6126dfc1dd446b7e9e0154f6e
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-22 17:05:17 +0530

    Updated

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ print('Hello ', name)
 for i in name:
     print(i)
 desc = "Hello Miss Sujata!"
-# # print(desc[3])
+print(desc[3])
 # txt = "Hello sujata ji, hope you are doing well!"
 # print('sujata' in txt)
 # if 'seed' in txt:
