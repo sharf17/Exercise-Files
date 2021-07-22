c0f3da4a7d00bef06fe637f6c1c38e9190a33821
sharf17 <mohammedsharfuddin390@gmail.com>
2021-07-22 17:05:55 +0530

    Updated

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ desc = "Hello Miss Sujata!"
 print(desc[3])
 txt = "Hello sujata ji, hope you are doing well!"
 print('sujata' in txt)
-# if 'seed' in txt:
+if 'seed' in txt:
 #     print('sujata')
 # else:
 #     print('correct your element')
