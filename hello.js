410e4233f93b908fd112fbc144a8d45ffc4837bd
sharf17 <mohammedsharfuddin390@gmail.com>
2021-08-21 23:33:13 +0530

    Updated

--- hello.js
+++ hello.js
@@ -4,12 +4,14 @@
 // let b = 'sharf'
 // d = c+b
 // console.log(d)
-// console.log(typeof(d))
-let txt = "Apple, Banana, Carrot, DogeCoin, dogeCoin"
-console.log(txt.slice(7,13))
-console.log(txt.substring(0,13))
-console.log(txt.substr(0,1))
-console.log(txt.replace('DogeCoin', "Dog"))
-console.log(txt.replace('dogeCoin', "Dog"))
-console.log(txt.replace(/DogeCoin/i, "Dog"))
-console.log(txt.replace(/DogeCoin/gi, "Dog"))
+// // console.log(typeof(d))
+// let txt = "Apple, Banana, Carrot, DogeCoin, dogeCoin"
+// console.log(txt.slice(7,13))
+// console.log(txt.substring(0,13))
+// console.log(txt.substr(0,1))
+// console.log(txt.replace('DogeCoin', "Dog"))
+// console.log(txt.replace('dogeCoin', "Dog"))
+// console.log(txt.replace(/DogeCoin/i, "Dog"))
+// console.log(txt.replace(/DogeCoin/gi, "Dog"))
+let txt = "aaa"
+console.log(txt.slice(0,2))
