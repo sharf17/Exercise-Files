d116b135690fe0b3a80566adad8169653ccc94f4
sharf17 <mohammedsharfuddin390@gmail.com>
2021-08-21 23:04:41 +0530

    Updated

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 // let a = "Hello Wolrd!"
 // console.log(a[2])
-let c = 10
-let b = 'sharf'
-d = c+b
-console.log(d)
-console.log(typeof(d))
+// let c = 10
+// let b = 'sharf'
+// d = c+b
+// console.log(d)
+// console.log(typeof(d))
+let txt = "Apple, Banana, Carrot, DogeCoin"
+console.log(txt.slice(7,13))
+console.log(txt.substring(0,13))
--- hello.py
+++ hello.py
@@ -1,38 +1,30 @@
-name = input("What should you be called as?")
-print('Hello ', name)
-for i in name:
-    print(i)
-desc = "Hello Miss Sujata!"
-print(desc[3])
-txt = "Hello sujata ji, hope you are doing well!"
-print('sujata' in txt)
-if 'seed' in txt:
-    print('sujata')
-else:
-    print('correct your element')
-print('a')
-a = "    Sujata Rajaram Pujari!"
-print(a.strip())
-a = 10
-b = 'sujju {}'
-print (b.format(a))
-a = 'heelo world'
-print(a.split())
-# name = 'sujata' 
-# age = 23 
-place = 'ajanale'
-# detail = 'Hey I am {2}, I am {1} years old and I live at {0}'
-# print(detail.format(place,age,name))
-"Hey we are Indian's"
-# a = name.upper()
-print(name.count('a'))
-# print(name.encode())
-# print(name.endswith('a'))
-# print(name.find('s'))
-# print(name.expandtabs(5))
-# b = a.casefold()
-# print(b)
-# c = a.lower()
-# print(c)
-print('Hello World!!!') #printing output
-print('Thanks for visiting, stay safe!')
+import sys
+
+def rightRotation(a, d):
+    out = list(a)
+    a_len = len(a)
+    for ind, el in enumerate(a):
+        out[(ind + d) % a_len] = el
+
+    return out
+
+def circularArrayRotation(a, m):
+    out = []
+    for pos in m:
+        out.append(a[pos])
+        
+    return out
+    
+
+if __name__ == "__main__":
+    n, k, q = input().strip().split(' ')
+    n, k, q = [int(n), int(k), int(q)]
+    a = list(map(int, input().strip().split(' ')))
+    m = []
+    m_i = 0
+    for m_i in range(q):
+        m_t = int(input().strip())
+        m.append(m_t)
+    a = rightRotation(a, k)
+    result = circularArrayRotation(a, m)
+    print ("\n".join(map(str, result)))
