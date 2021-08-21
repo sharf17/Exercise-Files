d3c5ec288336e638b4f8e57ce0f6f88555649223
sharf17 <mohammedsharfuddin390@gmail.com>
2021-08-21 23:08:10 +0530

    Updated

--- hello.js
+++ hello.js
@@ -7,5 +7,5 @@
 // console.log(typeof(d))
 let txt = "Apple, Banana, Carrot, DogeCoin"
 console.log(txt.slice(7,13))
-// console.log(txt.substring(0,13))
+console.log(txt.substring(0,13))
 // console.log(txt.substr(0,1))
