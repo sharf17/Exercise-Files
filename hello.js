10b5d96094acea94032959abf4f78a6c7e08d0f3
sharf17 <mohammedsharfuddin390@gmail.com>
2021-08-22 22:41:08 +0530

    Updated

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@
 // console.log(a[2])
 // let c = 10
 // let b = 'sharf'
-// d = c+b
+d = c+b
 // console.log(d)
 // // console.log(typeof(d))
 // let txt = "Apple, Banana, Carrot, DogeCoin, dogeCoin"
