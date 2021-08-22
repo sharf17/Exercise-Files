75e2e8b333f94569a667eb692b26ba7f371fee45
sharf17 <mohammedsharfuddin390@gmail.com>
2021-08-22 22:42:35 +0530

    Updated

--- hello.js
+++ hello.js
@@ -11,7 +11,7 @@ console.log(txt.substring(0,13))
 console.log(txt.substr(0,1))
 console.log(txt.replace('DogeCoin', "Dog"))
 console.log(txt.replace('dogeCoin', "Dog"))
-// console.log(txt.replace(/DogeCoin/i, "Dog"))
+console.log(txt.replace(/DogeCoin/i, "Dog"))
 // console.log(txt.replace(/DogeCoin/gi, "Dog"))
 // let txt = "aaa"
 // console.log(txt.slice(0,2))
