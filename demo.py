83c303ac0ace61789cef3bd4526f9d9910f6a56a
sharf17 <mohammedsharfuddin390@gmail.com>
2021-08-23 13:46:27 +0530

    Updated

--- demo.py
+++ demo.py
@@ -1,6 +1,13 @@
-a = 10
-print(type(a))
-print(id(a))
-print(bin(a))
-print(oct(a))
-print(hex(a))
+# a = 10
+# print(type(a))
+# print(id(a))
+# print(bin(a))
+# print(oct(a))
+# print(hex(a)) malli clear ga chpu loop
+a = int(input())  
+l = []
+for i in range(a): 
+    b = int(input())
+    # print(b)
+    l.append(b)
+print(l)
--- hello.js
+++ hello.js
@@ -1,17 +1,20 @@
-let a = "Hello Wolrd!"
-console.log(a[2])
-let c = 10
-let b = 'sharf'
-d = c+b
-console.log(d)
-console.log(typeof(d))
-let txt = "Apple, Banana, Carrot, DogeCoin, dogeCoin"
-console.log(txt.slice(7,13))
-console.log(txt.substring(0,13))
-console.log(txt.substr(0,1))
-console.log(txt.replace('DogeCoin', "Dog"))
-console.log(txt.replace('dogeCoin', "Dog"))
-console.log(txt.replace(/DogeCoin/i, "Dog"))
-console.log(txt.replace(/DogeCoin/gi, "Dog"))
-let txt = "aaa"
-console.log(txt.slice(0,2))
+// let a = "Hello Wolrd!"
+// console.log(a[2])
+// let c = 10
+// let b = 'sharf'
+// d = c+b
+// console.log(d)
+// console.log(typeof(d))
+// let txt = "Apple, Banana, Carrot, DogeCoin, dogeCoin"
+// console.log(txt.slice(7,13))
+// console.log(txt.substring(0,13))
+// console.log(txt.substr(0,1))
+// console.log(txt.replace('DogeCoin', "Dog"))
+// console.log(txt.replace('dogeCoin', "Dog"))
+// console.log(txt.replace(/DogeCoin/i, "Dog"))
+// console.log(txt.replace(/DogeCoin/gi, "Dog"))
+// let txt = "aaa"
+// console.log(txt.slice(0,2))
+let a = [10,12,14]
+a = ['a', 10, 12, 14]
+console.log(a)
